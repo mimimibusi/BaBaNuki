83e6e16c6058e73243d6449c3f6e707118790095
NaoyaFurumura <hurumura1120@icloud.com>
2021-09-26 19:17:54 +0900

    create gameController

---
+++ gameController.js
@@ -0,0 +1,9 @@
+export const createGame = (req, res) => {
+  //ここで、フロントエンドから、送られたplayerを取得する
+  const players = req.body.players;
+  /*
+
+    手札作成処理をかく
+    */
+  return "手札";
+};
--- index.js
+++ index.js
@@ -1,45 +1,48 @@
-var _ = require('lodash');
-var express = require('express');
+import { createGame } from "./gameController";
+var _ = require("lodash");
+var express = require("express");
 var app = express();
 var yamahuda = [];
 var player_Number = 3;
 
-app.get('/', (req, res)=>{
-    res.sendFile(__dirname + '/home.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/home.html");
 });
 
+//gameを作成する時のcontroller
+app.post("/game/", createGame);
 
-[...Array(13)].map((el,i)=>{
-    _.times(4,()=>{yamahuda.push(i+1)})
+[...Array(13)].map((el, i) => {
+  _.times(4, () => {
+    yamahuda.push(i + 1);
+  });
 });
 //1~54の数字を作っている
-yamahuda.push('Joker');
+yamahuda.push("Joker");
 
-
-function shuffle(n){
-    var newArray = [];
-    while(n.length > 0){
-        var k = Math.floor(Math.random() * n.length);
-        newArray.push(n[k]);
-        n.splice(k, 1);
-    }
-    return newArray;
+function shuffle(n) {
+  var newArray = [];
+  while (n.length > 0) {
+    var k = Math.floor(Math.random() * n.length);
+    newArray.push(n[k]);
+    n.splice(k, 1);
+  }
+  return newArray;
 }
 
 var shuffle_yamahuda = shuffle(yamahuda);
 var tehuda = {};
-var player_Name = 'たろう';
+var player_Name = "たろう";
 tehuda[player_Name] = [];
 
-var　maisuu = Math.floor(shuffle_yamahuda.length / player_Number);
-for(var i = 0;i < maisuu; i++){
-    tehuda[player_Name].push(shuffle_yamahuda[i]);
-    shuffle_yamahuda.splice(i, 1);
+var maisuu = Math.floor(shuffle_yamahuda.length / player_Number);
+for (var i = 0; i < maisuu; i++) {
+  tehuda[player_Name].push(shuffle_yamahuda[i]);
+  shuffle_yamahuda.splice(i, 1);
 }
 console.log(tehuda[player_Name].length);
 console.log(maisuu);
 
-
-var server = app.listen(3000, ()=>{
-    console.log('Server is running!');
-})
+var server = app.listen(3000, () => {
+  console.log("Server is running!");
+});
