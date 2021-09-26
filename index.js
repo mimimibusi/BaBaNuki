45e3b9189649429df71007c6ce41b69d0d0f7245
大塚みちあ <ootsukamichia@ootsukamichianoMacBook.local>
2021-09-26 17:24:12 +0900

    shuffle a-nd tarou ni kubaru

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
 var _ = require('lodash');
+var express = require('express');
+var app = express();
 var yamahuda = [];
+var player_Number = 3;
 
-/*for(n = 0; n <=3; n++){
-    for(i = 1; i <= 13; i++){
-        yamahuda.push(i);
-    };
-} */
-console.log(yamahuda);
-console.log([...Array(13)]);
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/home.html');
+});
+
+
 [...Array(13)].map((el,i)=>{
-    //console.log(el);
     _.times(4,()=>{yamahuda.push(i+1)})
 });
-//console.log(yamahuda);
+//1~54の数字を作っている
+yamahuda.push('Joker');
 
 
+function shuffle(n){
+    var newArray = [];
+    while(n.length > 0){
+        var k = Math.floor(Math.random() * n.length);
+        newArray.push(n[k]);
+        n.splice(k, 1);
+    }
+    return newArray;
+}
+
+var shuffle_yamahuda = shuffle(yamahuda);
+var tehuda = {};
+var player_Name = 'たろう';
+tehuda[player_Name] = [];
+
+var　maisuu = Math.floor(shuffle_yamahuda.length / player_Number);
+for(var i = 0;i < maisuu; i++){
+    tehuda[player_Name].push(shuffle_yamahuda[i]);
+    shuffle_yamahuda.splice(i, 1);
+}
+console.log(tehuda[player_Name].length);
+console.log(maisuu);
+
+
+var server = app.listen(3000, ()=>{
+    console.log('Server is running!');
+})
